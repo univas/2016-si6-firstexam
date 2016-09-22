6447c820d103ef2dd7b06d2e2b547abf506f9bab
Alison Rafael <armg.alison@gmail.com>
2016-09-22 20:32:10 -0300

    Finished Question 01, but need to remove NaN

--- Alison Rafael/Question 01/oddEvenIdentify.js
+++ Alison Rafael/Question 01/oddEvenIdentify.js
@@ -1,20 +1,43 @@
 function OddEvenIdentify(){
     
-    function initialize() {
-        
-    }
+    (function initialize() {
+        displayResult(mountResult(getArgv()));
+    })();
     
     function getArgv(){
-        return process.argv.slice(2).map(num => parseFloat(num));
+        return process.argv.slice(2).map(num => parseInt(num));
     }
     
     function isANumber(number){
-        return typeof(number) == 'number' ? true : false;
+        console.log(typeof(number));
+        return typeof(number) === 'number' ? true : false;
     }
     
     function isEven(number){
-        return number %2 == 0 ? true : false;
+        return number %2 == 0 ? 'even' : 'odd';
     }
     
-    this.initialize = initialize;
+    function mountResult(arrayNumbers) {
+        var resultObject = { 'numbers' : [] };
+        arrayNumbers.forEach(function (numb) {
+            if(isANumber(numb)){
+                var oddEvenObject = new Object();
+                oddEvenObject.number = numb;
+                oddEvenObject.type = isEven(numb);
+                resultObject.numbers.push(oddEvenObject);
+            }
+        });
+        return resultObject;
+    }
+    
+    function displayResult(result){
+        console.log('NUMBERS:\n');
+        result.numbers.forEach(function (oddEven) {
+            console.log('type: ' + oddEven.type + '\tnumber:' + oddEven.number);
+        });
+    }
+    
 }
+
+var oddEvenIdentify = new OddEvenIdentify();
+//oddEvenIdentify.initialize();
