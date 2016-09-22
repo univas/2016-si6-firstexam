abdca2281bb3641adbeb30326b73466381300031
Auro Mota <aurofmota@gmail.com>
2016-09-22 19:40:01 -0300

    Question 2

---
+++ auromota/calculate-offer.js
@@ -0,0 +1,9 @@
+let price = parseFloat(process.argv[2]);
+let discount = process.argv[3];
+let discountValue = parseFloat(discount.replace('%', ''));
+
+function calculatePrice(price, discountValue) {
+    return price - price * discountValue / 100;
+}
+
+console.log('O valor final com ' + discount + ' de desconto é ' + calculatePrice(price, discountValue).toFixed(2));
