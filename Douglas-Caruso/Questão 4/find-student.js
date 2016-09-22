4fa77ec7dbfd681cdf60fe9af5241608d448a8e0
Aluno0323 <Aluno0323@univas.edu.br>
2016-09-22 20:36:33 -0300

    added functions to Question 4

--- Douglas-Caruso/Questão 4/find-student.js
+++ Douglas-Caruso/Questão 4/find-student.js
@@ -1,9 +1,20 @@
 const studentsDB = require("./students.json");
-const matricula = process.argv[2];
+start();
 
-for (var i = 0; i < studentsDB.length; i++){
-    if (studentsDB[i].mat == matricula){
-        console.log("Nome: " + studentsDB[i].name);
-        console.log("Email: " + studentsDB[i].email);
+function start(){
+    const matricula = process.argv[2];
+    printData(findStudent(matricula));
+}
+
+function findStudent(matricula) {
+    for (var i = 0; i < studentsDB.length; i++){
+        if (studentsDB[i].mat == matricula)
+            return studentsDB[i];
     }
 }
+
+function printData(student) {
+    console.log("Nome: " + student.name);
+    console.log("Email: " + student.email);
+}
+
