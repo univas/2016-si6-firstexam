33b7b820be49ea10a9a341a10750bccfe3651666
Aluno0321 <Aluno0321@univas.edu.br>
2016-09-22 20:30:41 -0300

    commit questao 3

---
+++ Marcos/questao3/define-abbreviation.js
@@ -0,0 +1,31 @@
+
+
+var nome = process.argv[2];
+
+if(nome == "JS"){
+    console.log("JS - JavaScript");
+}
+
+if(nome == "HTML"){
+    console.log("HTML - HyperText Markup Language");
+    
+}
+
+if(nome == "CSS"){
+    
+    console.log("CSS - Cascading Style Sheets");
+}
+
+if(nome == "FTP"){
+    console.log("FTP - File Transfer Protocol");
+}
+
+if(nome=="HTTP"){
+    console.log("HTTP - The Hypertext Transfer Protocol");
+}
+
+if(nome == "TDD"){
+    
+    console.log("TDD - Test Driven Development");
+}
+
