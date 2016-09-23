0ffb94c1212a8c092dd365feb6bcf181d2cb5b69
Ariane <ariane.prii@gmail.com>
2016-09-22 22:28:06 -0300

    alterando login

---
+++ Ariane/findStudents.js
@@ -0,0 +1,39 @@
+//find-students
+
+
+arraydeEstudantes = [{'id':'98007865'},{'id':'9003063'},{'id':'98008543'},{'id':'98007380'}]
+
+
+
+function procurarEstudantes() {
+    var id;
+    console.log("Informe  a matricula do estudante ");
+     if(numbers.length > 1) {
+        console.log("Informe uma matricula só!");
+}
+    
+        
+    }
+    
+if (id==98007865){
+    console.log(" Alisson Rafael Marinho Gonçalves, armg.alisson.gmail.com");
+    
+}
+if(id ==98003063){
+    console.log("Ariane Presciliana Moreira,ariane_prii@hotmail.com");
+}
+
+if(id == 98008543){
+    console.log("Auro Felipe Sampaio Mota,palubo01@gmail.com");
+    
+}
+
+function getArguments(){
+    
+ return  process.argv[2]; 
+}
+
+function stringToNumber(stringNumber) {
+    return parseFloat(stringNumber);
+}
+
