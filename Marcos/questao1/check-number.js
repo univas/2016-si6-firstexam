cd7d096b2b631dc6c6515adae37a26d110b8cd8c
Aluno0321 <Aluno0321@univas.edu.br>
2016-09-22 21:12:24 -0300

    commit questao 1 again

--- Marcos/questao1/check-number.js
+++ Marcos/questao1/check-number.js
@@ -4,8 +4,8 @@
     
     if(numero%2===0){
         
-        console.log("O número"+ numero+ " é um número par");
+        console.log("O número " + numero+ " é um número par");
     }else{
         
-        console.log("O número" + numero + "é um número ímpar");
+        console.log("O número " + numero + " é um número ímpar");
     }
