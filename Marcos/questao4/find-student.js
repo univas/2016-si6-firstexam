28d79afb6f056f3267f4452136ade76958902cc2
Aluno0321 <Aluno0321@univas.edu.br>
2016-09-22 21:10:22 -0300

    commit questao 4

---
+++ Marcos/questao4/find-student.js
@@ -0,0 +1,91 @@
+
+var numero = process.argv[2];
+
+
+if(numero == 98007865){
+    console.log("Nome: Alison Rafael Marinho Gonçalves \n email: armg.alison@gmail.com");
+}
+
+if(numero == 98003063){
+    console.log("Nome: Ariane Presciliana Moreira \n	email: ariane_prii@hotmail.com");
+}
+
+if(numero == 98008543){	
+    console.log("Nome: Auro Felipe Sampaio Mota	 \n email: palubo01@gmail.com");
+}
+
+  if(numero == 98007380){
+      console.log("Nome: Douglas de Cássio Oliveira \n	email: douglinhas_uchiha@hotmail.com");
+  }
+  
+  
+if(numero == 98007574){
+    console.log("Nome: Douglas Eduardo Caruso \n	email: doug-caruso@hotmail.com");
+}
+
+if(numero == 98007049){
+    console.log("Nome: Elenice Carla Assis \n	email: elenice.cassis@gmail.com");
+}
+
+if(numero == 98008002){
+    console.log("Nome: Fabiano Gonçalves Lomonaco Júnior \n email: fabiano.lomonaco.junior@gmail.com");
+}
+
+if(numero == 98007381){
+    console.log("Nome: Gustavo Henrique Martins	\n email: gustavo_sh8@hotmail.com");
+}
+
+if(numero == 98001244){
+    console.log("Nome: Jefferson Leon Pereira	\n email: jeffersonleon310@yahoo.com.br");
+}
+
+if(numero == 98007580){
+    console.log("Nome: Leandro Gabriel Garcia	\n email: leandrolg13@yahoo.com.br");
+}
+
+if(numero == 98007858){
+    console.log("Nome: Luiz Eduardo da Costa \n email: costaeduardoluiz@gmail.com");
+}
+
+if( numero == 98007071){
+    console.log("Nome: Marcos Antonio Rogel Junior \n	email: junior_bs192@yahoo.com.br");
+}
+
+if(numero == 98008063){
+    console.log("Nome: Mateus Ferreira de Souza	\n email: seraomateus@hotmail.com");
+    
+}
+
+if(numero == 98007350){
+    console.log("Nome: Matheus José Vieira Lemes \n email: matheus_lemes1996@hotmail.com");
+    
+}
+
+if(numero == 98002394){
+    console.log("Nome: Maycon Assis Fonseca Ferreira	\n email: maycon.univas@gmail.com");
+    
+}
+
+if(numero == 98004513){
+    console.log("Nome: Rodrigo Neves Ottoboni Dias	\n email: rodrigon@fuvs.br");
+}
+
+if(numero == 13001952){
+    console.log("Nome: Sabrina Andrade Pereira	\n email: saandradepereira@gmail.com");
+}
+
+if(numero == 98007252){
+  console.log("Nome: Samuel Barreiro Lemes	\n email: samuellemes25@gmail.com");
+}
+
+if(numero == 98007985){
+    console.log("Nome: Simao Faustino Laudino	\n email: simao-pf@hotmail.com");
+}
+
+if(numero == 98007494){
+    console.log("Nome: Wellington da Cruz Caldeira	\n email: well.caldeira@gmail.com");
+}
+
+if(numero == 98007587){
+    console.log("Nome: Wellington de Jesus Braga	\n email: wellington.braga.inatel@gmail.com");
+}
