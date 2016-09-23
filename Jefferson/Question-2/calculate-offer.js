1d3848357125f99ca3ebf9d4936dcdc6e5328796
JeffLePe <jeffersonleon310@yahoo.com.br>
2016-09-22 22:22:40 -0300

    modified file calculate-offer.js

--- Jefferson/Question-2/calculate-offer.js
+++ Jefferson/Question-2/calculate-offer.js
@@ -1,7 +1,7 @@
-process.argv[2];
-process.argv[3];
+var money = process.argv[2];
+var disc = process.argv[3];
 
-console.log("O valor final com " + process.argv[3] + " de desconto é " + calcDiscount(process.argv[2], process.argv[3]));
+console.log("O valor final com " + process.argv[3] + " de desconto é " + calcDiscount(money, disc));
 
 function calcDiscount(value, disc) {
     valueMoney = parseFloat(value);
