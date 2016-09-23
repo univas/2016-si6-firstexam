b5b3ddfd67b83bebf2fb749ba36f6b70852e89c4
Fabiano <fabiano.lomonaco.junior@gmail.com>
2016-09-22 21:05:19 -0300

    expression correction

--- Fabiano_Lomonaco_SI6/2/calculate-offer.js
+++ Fabiano_Lomonaco_SI6/2/calculate-offer.js
@@ -2,6 +2,7 @@
 var valor = process.argv.slice(2);
 var porc = parseFloat(process.argv.slice(2));
 
-var total = valor * (porc/100);
+var total;
+total = valor - (valor * (porc/100));
 
 console.log("O valor final e: " + total);
