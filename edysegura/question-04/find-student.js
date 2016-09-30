d42c713cc61cacd6f9f0c7c5528ee415377eecb9
Edy Segura <edysegura@gmail.com>
2016-09-29 22:22:12 -0300

    Final version for find-students.js

--- edysegura/question-04/find-student.js
+++ edysegura/question-04/find-student.js
@@ -4,15 +4,21 @@ function getStudentCode() {
 
 function findStudent(code) {
     var students = require('./studentsDB.json')
-    var student = students.find(function search(student) {
+    var searchStudent = function(student) {
         return student.code === code
-    });
+    }
+    var student = students.find(searchStudent)
     return student
 }
 
 (function main(){
     var code = getStudentCode()
     var student = findStudent(code)
-    console.log("Name: " + student.name)
-    console.log("Email: " + student.email)
+    if (student) {
+        console.log("Name: " + student.name)
+        console.log("Email: " + student.email)
+    }
+    else {
+        console.log("Student not found!")
+    }
 })()
