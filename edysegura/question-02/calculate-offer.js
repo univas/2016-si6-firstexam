f535eff5cb9bb6c0567a402061259ce0b335545a
Edy Segura <edysegura@gmail.com>
2016-09-29 21:54:31 -0300

    The final version for calculate-offer.js

--- edysegura/question-02/calculate-offer.js
+++ edysegura/question-02/calculate-offer.js
@@ -1,15 +1,29 @@
 function getPrice() {
-    return process.argv[2]
+    return parseFloat(process.argv[2])
 }
 
 function getPercentage() {
-    return process.argv[3]
+    return parseFloat(process.argv[3])
 }
 
 function calculateDiscount(price, discountPercentage) {
-    return 0;
+    return price * ((100 - discountPercentage) / 100)
 }
 
-console.log("Price: " + getPrice())
-console.log("Percentage: " + getPercentage())
-console.log("Final price: " + calculateDiscount())
+function isNumber(number) {
+    return !isNaN(number)
+}
+
+(function main() {
+    var price = getPrice()
+    var discountPercentage = getPercentage()
+    var finalPrice = 0
+    var message = "Please inform valid values such as 1799.99 15%"
+    
+    if(isNumber(price) && isNumber(discountPercentage)) {
+        finalPrice = calculateDiscount(price, discountPercentage).toFixed(2);
+        message = "The final value with " + discountPercentage + "% is " + finalPrice
+    }
+     
+    console.log(message)
+})()
