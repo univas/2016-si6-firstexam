f923f8337572be5908cc136906610e9e2197f079
JuniorR192 <junior_bs192@yahoo.com.br>
2016-10-02 19:19:28 -0300

    Marcos' exam (#8)
    * commit questao 1
    
    * commit questao 3
    
    * commit questao 2
    
    * commit questao 4
    
    * commit questao 1 again

---
+++ Marcos/questao1/check-number.js
@@ -0,0 +1,11 @@
+
+    
+    var numero= parseFloat(process.argv[2]);
+    
+    if(numero%2===0){
+        
+        console.log("O número " + numero+ " é um número par");
+    }else{
+        
+        console.log("O número " + numero + " é um número ímpar");
+    }
